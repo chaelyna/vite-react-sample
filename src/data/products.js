c7f2e8a43ada9ac7701c50c95c5f50273637ab1a
chaelyna <rlflsdl32@naver.com>
2025-08-12 16:23:24 +0900

    images 경로 수정

--- src/data/products.js
+++ src/data/products.js
@@ -1,13 +1,13 @@
 const products = [
-    { id: 'toaster', name: '토스터기', desc: '유니버셜 미니언즈 토스터기', price: 45900, img: '/images/토스터기.png' },
-    { id: 'handfan', name: '핸디선풍기', desc: '미니언즈 휴대용 저소음 핸디선풍기', price: 27900, img: '/images/핸디선풍기.png' },
-    { id: 'monitor-perfume', name: '모니터 방향제', desc: '유니버셜 미니언즈 모니터 방향제', price: 22000, img: '/images/모니터 방향제.png' },
-    { id: 'blocky', name: '블로키 미니언즈', desc: '블로키 미니언즈 슈트어트 PC03', price: 12000, img: '/images/블로키 미니언즈.png' },
+    { id: 'toaster', name: '토스터기', desc: '유니버셜 미니언즈 토스터기', price: 45900, img: './images/토스터기.png' },
+    { id: 'handfan', name: '핸디선풍기', desc: '미니언즈 휴대용 저소음 핸디선풍기', price: 27900, img: './images/핸디선풍기.png' },
+    { id: 'monitor-perfume', name: '모니터 방향제', desc: '유니버셜 미니언즈 모니터 방향제', price: 22000, img: './images/모니터 방향제.png' },
+    { id: 'blocky', name: '블로키 미니언즈', desc: '블로키 미니언즈 슈트어트 PC03', price: 12000, img: './images/블로키 미니언즈.png' },
 
-    { id: 'lego-minions', name: '레고 미니언즈', desc: '미니언즈와 바나나 카/레고 정품', price: 34900, img: '/images/레고 미니언즈.png' },
-    { id: 'lucky-pollack', name: '액막이 명태', desc: '행운과 평안을 기원한 피규어형 액막이', price: 20000, img: '/images/액막이.png' },
-    { id: 'battery', name: '보조배터리', desc: '춘식이 고속충전 보조배터리 (10000mAh)', price: 36900, img: '/images/배터리.png' },
-    { id: 'vent-figure', name: '송풍구 피규어', desc: '안전운전 기원 냥냥 피규어 키링', price: 12000, img: '/images/새차.png' },
+    { id: 'lego-minions', name: '레고 미니언즈', desc: '미니언즈와 바나나 카/레고 정품', price: 34900, img: './images/레고 미니언즈.png' },
+    { id: 'lucky-pollack', name: '액막이 명태', desc: '행운과 평안을 기원한 피규어형 액막이', price: 20000, img: './images/액막이.png' },
+    { id: 'battery', name: '보조배터리', desc: '춘식이 고속충전 보조배터리 (10000mAh)', price: 36900, img: './images/배터리.png' },
+    { id: 'vent-figure', name: '송풍구 피규어', desc: '안전운전 기원 냥냥 피규어 키링', price: 12000, img: './images/새차.png' },
 ];
 
 export default products;
